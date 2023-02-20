7d28b6547059d5165b35b02d6b8cb6dd638b3d5c
ahfasxp1 <ahfas.fazria@gmail.com>
2023-02-20 15:21:53 +0700

    Initial Commit

---
+++ The Shop/The_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_ShopApp.swift
+//  The Shop
+//
+//  Created by OjekBro - Ahfas on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct The_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
