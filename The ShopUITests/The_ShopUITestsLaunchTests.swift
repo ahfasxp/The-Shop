64be4f9e20fd95da3bfa7efd867c3049cb8b84b7
ahfasxp1 <ahfas.fazria@gmail.com>
2023-03-17 09:28:31 +0700

    add target test

---
+++ The ShopUITests/The_ShopUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  The_ShopUITestsLaunchTests.swift
+//  The ShopUITests
+//
+//  Created by OjekBro - Ahfas on 17/03/23.
+//
+
+import XCTest
+
+final class The_ShopUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
